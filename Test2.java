2139063d498ceb6569882af337f58f6205856b5c
Shubham <shubhamkokode11697@gmail.com>
2023-02-16 21:47:09 +0530

    COMMITING

---
+++ Test2.java
@@ -0,0 +1,8 @@
+class Test2{
+    public void guys(){
+        System.out.println("Hi guys");
+    }
+    public static void main(String args[]){
+        System.out.println("Welcome coder");
+    }
+}
